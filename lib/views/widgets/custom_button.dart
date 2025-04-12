d6969449ee29284b7f3e9413c6c860fd4e634cce
Your Name <your.email@example.com>
2025-04-12 18:08:39 +0200

    take input and validate data

--- lib/views/widgets/custom_button.dart
+++ lib/views/widgets/custom_button.dart
@@ -2,23 +2,28 @@ import 'package:flutter/material.dart';
 import 'package:notes_app/contant.dart';
 
 class CustomButton extends StatelessWidget {
-  const CustomButton({super.key});
+  const CustomButton({super.key,this.onTap});
+
+ final void Function()? onTap;
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: MediaQuery.of(context).size.width,
-      height: 55,
-      decoration: BoxDecoration(
-        color: kPrimaryColor,
-        borderRadius: BorderRadius.circular(8),
-      ),
-      child: Center(
-        child: Text(
-          'Add',
-          style: TextStyle(
-              color: Colors.black,
-              fontWeight: FontWeight.bold
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        width: MediaQuery.of(context).size.width,
+        height: 55,
+        decoration: BoxDecoration(
+          color: kPrimaryColor,
+          borderRadius: BorderRadius.circular(8),
+        ),
+        child: Center(
+          child: Text(
+            'Add',
+            style: TextStyle(
+                color: Colors.black,
+                fontWeight: FontWeight.bold
+            ),
           ),
         ),
       ),
