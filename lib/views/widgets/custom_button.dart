c05043be36c25f6b201511bddff31ca3522245cc
Your Name <your.email@example.com>
2025-04-15 15:28:26 +0200

    change add note bottom sheet loading

--- lib/views/widgets/custom_button.dart
+++ lib/views/widgets/custom_button.dart
@@ -2,9 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:notes_app/contant.dart';
 
 class CustomButton extends StatelessWidget {
-  const CustomButton({super.key,this.onTap});
+  const CustomButton({super.key, this.onTap, this.isLoading = false});
 
- final void Function()? onTap;
+  final void Function()? onTap;
+  final bool isLoading;
 
   @override
   Widget build(BuildContext context) {
@@ -18,13 +19,19 @@ class CustomButton extends StatelessWidget {
           borderRadius: BorderRadius.circular(8),
         ),
         child: Center(
-          child: Text(
-            'Add',
-            style: TextStyle(
-                color: Colors.black,
-                fontWeight: FontWeight.bold
-            ),
-          ),
+          child: isLoading
+              ? SizedBox(
+                  height: 24,
+                  width: 24,
+                  child: const CircularProgressIndicator(
+                    color: Colors.black,
+                  ),
+                )
+              : const Text(
+                  'Add',
+                  style: TextStyle(
+                      color: Colors.black, fontWeight: FontWeight.bold),
+                ),
         ),
       ),
     );
