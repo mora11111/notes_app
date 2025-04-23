2ea0d9f982cbc91c98a89c94e1fa8021e43baf99
Your Name <your.email@example.com>
2025-04-23 09:33:50 +0200

    delete note done

--- lib/views/widgets/custom_note_item.dart
+++ lib/views/widgets/custom_note_item.dart
@@ -62,7 +62,9 @@ class NoteItem extends StatelessWidget {
                 ),
               ),
               trailing: IconButton(
-                onPressed: () {},
+                onPressed: () {
+                  note.delete();
+                },
                 icon: Icon(
                   FontAwesomeIcons.trash,
                   color: Colors.black,
